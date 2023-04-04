4b02c356560e2c162c5fa3a95ae1c04cdb477303
Caligula <noe.marcaillou@epitech.eu>
2023-04-05 01:57:44 +0200

    def

--- unit_tests/tests_run.c
+++ unit_tests/tests_run.c
@@ -1,19 +1,5 @@
 #include <criterion/criterion.h>
-
 #include <stdio.h>
-
-int sum(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int a = 5;
-    int b = 7;
-    int result = sum(a, b);
-    printf("The sum of %d and %d is %d\n", a, b, result);
-    return 0;
-}
-
 
 Test(sum, returns_correct_sum) {
     int a = 5;
