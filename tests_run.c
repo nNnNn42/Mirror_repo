928d1c38cf8fe84bcd19b2f0adf125e076d909f9
Caligula <noe.marcaillou@epitech.eu>
2023-04-05 01:19:48 +0200

    tests fixed

--- tests_run.c
+++ tests_run.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+int main(int argc, char *argv[]) {
+
+    printf("Number of arguments: %d\n", argc);
+    
+ 
+    for (int i = 0;i<argc;i++)   {
+        printf("Argument %d: %s\n", i, argv[i]);   
+    }
+    
+    return 0;
+}
+
 Test(my_program, test_argument_count) {
     int argc = 4;
     char *argv[] = {"./program", "arg1", "arg2", "arg3"};
