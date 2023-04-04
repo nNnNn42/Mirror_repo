850a9abfefb62b0df100e858b3e9510b52665a46
Caligula <noe.marcaillou@epitech.eu>
2023-04-05 01:03:21 +0200

    p

---
+++ tests_run.c
@@ -0,0 +1,13 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "my_program.h"
+
+Test(my_program, returns_84_when_no_arguments)
+{
+    int argc = 1;
+    char *argv[] = {"my_program", NULL};
+    int result = my_program(argc, argv);
+    cr_assert_eq(result, 84, "Expected return value to be 84, but got %d", result);
+}
+
