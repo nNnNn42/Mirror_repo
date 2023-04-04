b2ee87a4b54f1bbe16b2703086a64fde07183611
Caligula <noe.marcaillou@epitech.eu>
2023-04-05 01:09:19 +0200

    p

--- my_chocolatine.c
+++ my_chocolatine.c
@@ -1,7 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main (void)
+int main(int argc, char *argv[])   
 {
+    if (argc > 1) {
     printf("coucou");
-    return 0;
+        return 0;
+    }
+                  
+    return 84;
 }
+
+
+
+
+
--- tests_run.c
+++ tests_run.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-Test(my_program, returns_84_when_no_arguments)
+Test(main, returns_84_when_no_arguments)
 {
     int argc = 1;
     char *argv[] = {"my_program", NULL};
-    int result = my_program(argc, argv);
+    int result = main(argc, argv);
     cr_assert_eq(result, 84, "Expected return value to be 84, but got %d", result);
 }
 
